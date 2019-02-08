642f1c57176567aed2d5da7a9732d9a5c51f60fd
Michael B <michael@ramyun.co.uk>
2019-02-08 10:04:46 +0000

    Initial raytracing attempts

--- raytracer/Source/skeleton.cpp
+++ raytracer/Source/skeleton.cpp
@@ -11,6 +11,7 @@ using glm::vec3;
 using glm::mat3;
 using glm::vec4;
 using glm::mat4;
+using glm::distance;
 
 SDL_Event event;
 
@@ -18,12 +19,15 @@ SDL_Event event;
 #define SCREEN_HEIGHT 256
 #define FULLSCREEN_MODE false
 
-struct Intersection
-{
+struct Intersection {
   vec4 position;
   float distance;
   int triangleIndex;
 };
+
+float focalLength = 1.0;
+vec4 cameraPos(0.0, 0.0, 0, 1.0);
+std::vector<Triangle> triangles;
 
 /* ----------------------------------------------------------------------------*/
 /* FUNCTIONS                                                                   */
@@ -33,105 +37,111 @@ void Draw(screen* screen);
 
 bool ClosestIntersection(vec4 start, vec4 dir, const vector<Triangle>& triangles, Intersection& closestIntersection);
 
-int main( int argc, char* argv[] )
-{
-  
+int main(int argc, char* argv[]) {
   screen *screen = InitializeSDL( SCREEN_WIDTH, SCREEN_HEIGHT, FULLSCREEN_MODE );
+  LoadTestModel(triangles);
 
-  while ( Update())
-    {
-      Draw(screen);
-      SDL_Renderframe(screen);
-    }
+  while (Update()) {
+    Draw(screen);
+    SDL_Renderframe(screen);
+  }
 
-  SDL_SaveImage( screen, "screenshot.bmp" );
+  SDL_SaveImage(screen, "screenshot.bmp");
 
   KillSDL(screen);
   return 0;
 }
 
 bool ClosestIntersection(vec4 start, vec4 dir, const vector<Triangle>& triangles, Intersection& closestIntersection) {
-  float m = std::numeric_limits<float>::max();
+  bool intersectionFound = false;
 
   for(std::vector<Triangle>::size_type i = 0; i < triangles.size(); i++) {
     vec4 v0 = triangles[i].v0;
     vec4 v1 = triangles[i].v1;
     vec4 v2 = triangles[i].v2;
 
-    vec3 e1 = vec3(v1.x-v0.x,v1.y-v0.y,v1.z-v0.z)
-    vec3 e2 = vec3(v2.x-v0.x,v2.y-v0.y,v2.z-v0.z)
-    vec3 b = vec3(s.x-v0.x,s.y-v0.y,s.z-v0.z);
+    vec3 e1 = vec3(v1.x-v0.x,v1.y-v0.y,v1.z-v0.z);
+    vec3 e2 = vec3(v2.x-v0.x,v2.y-v0.y,v2.z-v0.z);
+    vec3 b = vec3(start.x-v0.x,start.y-v0.y,start.z-v0.z);
+    vec3 d = vec3(dir.x, dir.y, dir.z);
 
-    mat3 A( -d, e1, e2 );
-    vec3 x = glm::inverse( A ) * b;
+    mat3 A = mat3(-d, e1, e2);
+    vec3 x = glm::inverse(A) * b;
 
     float t = x.x, u = x.y, v = x.z;
 
     if (u > 0 && v > 0 && (u+v) < 1 && t >= 0) {
       /* Intersection detected! */
-      // closestIntersection.position = ; TODO!
-      // closestIntersection.distance = ;
-      closestIntersection.triangleIndex = i;
-      return true;
+      vec4 position = vec4(t,u,v,1);
+      float dist = distance(start, position);
+
+      if (dist < closestIntersection.distance) {
+        closestIntersection.position = position;
+        closestIntersection.distance = dist;
+        closestIntersection.triangleIndex = i;
+        intersectionFound = true;
+      }
+
     }
   }
-  return false;
+  return intersectionFound;
 }
 
-/*Place your drawing here*/
-void Draw(screen* screen)
-{
+/* Place your drawing here */
+void Draw(screen* screen) {
   /* Clear buffer */
   memset(screen->buffer, 0, screen->height*screen->width*sizeof(uint32_t));
-  
-  vec3 colour(1.0,0.0,0.0);
-  for(int i=0; i<1000; i++)
-    {
-      uint32_t x = rand() % screen->width;
-      uint32_t y = rand() % screen->height;
+
+  for (int x = 0; x < SCREEN_WIDTH; x++) {
+    for (int y = 0; y < SCREEN_HEIGHT; y++) {
+      vec4 d = vec4(x - SCREEN_WIDTH/2, y - SCREEN_HEIGHT/2, focalLength, 1);
+      Intersection intersection;
+      intersection.distance = std::numeric_limits<float>::max();
+      vec3 colour(0.0, 0.0, 0.0); // Initialise to black
+
+      if (ClosestIntersection(cameraPos, d, triangles, intersection)) {
+        colour = triangles[intersection.triangleIndex].color;
+      }
+
       PutPixelSDL(screen, x, y, colour);
     }
+  }
 }
 
 /*Place updates of parameters here*/
-bool Update()
-{
+bool Update() {
   static int t = SDL_GetTicks();
   /* Compute frame time */
   int t2 = SDL_GetTicks();
   float dt = float(t2-t);
   t = t2;
 
+  std::cout << "Render time: " << dt << " ms." << std::endl;
+
   SDL_Event e;
-  while(SDL_PollEvent(&e))
-    {
-      if (e.type == SDL_QUIT)
-	{
-	  return false;
-	}
-      else
-	if (e.type == SDL_KEYDOWN)
-	  {
+  while(SDL_PollEvent(&e)) {
+    if (e.type == SDL_QUIT) {
+	    return false;
+	  } else if (e.type == SDL_KEYDOWN) {
 	    int key_code = e.key.keysym.sym;
-	    switch(key_code)
-	      {
+	    switch(key_code) {
 	      case SDLK_UP:
-		/* Move camera forward */
-		break;
+          /* Move camera forward */
+          break;
 	      case SDLK_DOWN:
-		/* Move camera backwards */
-		break;
+          /* Move camera backwards */
+          break;
 	      case SDLK_LEFT:
-		/* Move camera left */
-		break;
+          /* Move camera left */
+          break;
 	      case SDLK_RIGHT:
-		/* Move camera right */
-		break;
+          /* Move camera right */
+          break;
 	      case SDLK_ESCAPE:
-		/* Move camera quit */
-		return false;
-	      }
-	  }  
+          /* Move camera quit */
+          return false;
+      }
     }
+  }
   return true;
 }

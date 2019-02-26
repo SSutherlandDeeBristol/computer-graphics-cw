a07220aef357b661a6db3153ceae54e75eb969ce
SamSD <ss15060@my.bristol.ac.uk>
2019-02-26 16:43:36 +0000

    Fixed indenting

--- rasteriser/Source/skeleton.cpp
+++ rasteriser/Source/skeleton.cpp
@@ -23,16 +23,14 @@ SDL_Event event;
 bool Update();
 void Draw(screen* screen);
 
-int main( int argc, char* argv[] )
-{
-  
+int main( int argc, char* argv[] ) {
+
   screen *screen = InitializeSDL( SCREEN_WIDTH, SCREEN_HEIGHT, FULLSCREEN_MODE );
 
-  while ( Update())
-    {
-      Draw(screen);
-      SDL_Renderframe(screen);
-    }
+  while ( Update() ) {
+    Draw(screen);
+    SDL_Renderframe(screen);
+  }
 
   SDL_SaveImage( screen, "screenshot.bmp" );
 
@@ -41,23 +39,27 @@ int main( int argc, char* argv[] )
 }
 
 /*Place your drawing here*/
-void Draw(screen* screen)
-{
+void Draw(screen* screen) {
   /* Clear buffer */
   memset(screen->buffer, 0, screen->height*screen->width*sizeof(uint32_t));
-  
-  vec3 colour(1.0,0.0,0.0);
-  for(int i=0; i<1000; i++)
-    {
-      uint32_t x = rand() % screen->width;
-      uint32_t y = rand() % screen->height;
-      PutPixelSDL(screen, x, y, colour);
-    }
+
+  for( uint32_t i=0; i<triangles.size(); ++i ) {
+    vector<vec4> vertices(3);
+    vertices[0] = triangles[i].v0;
+    vertices[1] = triangles[i].v1;
+    vertices[2] = triangles[i].v2;
+    for(int v=0; v<3; ++v)
+     {
+       ivec2 projPos;
+       VertexShader( vertices[v], projPos );
+       vec3 color(1,1,1);
+       PutPixelSDL( screen, projPos.x, projPos.y, color );
+     }
+   }
 }
 
 /*Place updates of parameters here*/
-bool Update()
-{
+bool Update() {
   static int t = SDL_GetTicks();
   /* Compute frame time */
   int t2 = SDL_GetTicks();
@@ -65,35 +67,30 @@ bool Update()
   t = t2;
 
   SDL_Event e;
-  while(SDL_PollEvent(&e))
-    {
-      if (e.type == SDL_QUIT)
-	{
-	  return false;
-	}
-      else
-	if (e.type == SDL_KEYDOWN)
-	  {
-	    int key_code = e.key.keysym.sym;
-	    switch(key_code)
-	      {
+  while(SDL_PollEvent(&e)) {
+    if (e.type == SDL_QUIT) {
+      return false;
+    } else if (e.type == SDL_KEYDOWN) {
+      int key_code = e.key.keysym.sym;
+      switch(key_code) {
 	      case SDLK_UP:
-		/* Move camera forward */
-		break;
+          /* Move camera forward */
+          break;
 	      case SDLK_DOWN:
-		/* Move camera backwards */
-		break;
+		      /* Move camera backwards */
+		        break;
 	      case SDLK_LEFT:
-		/* Move camera left */
-		break;
+		      /* Move camera left */
+		        break;
 	      case SDLK_RIGHT:
-		/* Move camera right */
-		break;
+		      /* Move camera right */
+		        break;
 	      case SDLK_ESCAPE:
-		/* Move camera quit */
-		return false;
-	      }
-	  }  
+		      /* Move camera quit */
+		        return false;
+	    }
     }
+  }
+
   return true;
 }
